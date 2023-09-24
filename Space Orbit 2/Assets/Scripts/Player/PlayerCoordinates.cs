e3fa2069c0aad1953b7e053c8b24855c8e3588d2
OscarWirdelius <oscar.wirdelius@gmail.com>
2023-09-24 16:38:29 +0200

    added minimap & coordinates

---
+++ Space Orbit 2/Assets/Scripts/Player/PlayerCoordinates.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class PlayerCoordinates : MonoBehaviour
+{
+    [SerializeField] private string _currentCoordinates;
+
+    [SerializeField] private TMP_Text _displayText;
+
+    [SerializeField] private Transform _target;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _currentCoordinates = "[" + _target.position.x.ToString("F0") + ";" + _target.position.z.ToString("F0") + "]";
+        _displayText.text = _currentCoordinates;
+    }
+}
