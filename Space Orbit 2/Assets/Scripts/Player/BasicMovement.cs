c25f4b488fa7fce429319952e2c685798abb3e80
OscarWirdelius <oscar.wirdelius@gmail.com>
2023-09-30 23:07:31 +0200

    updated movement

--- Space Orbit 2/Assets/Scripts/Player/BasicMovement.cs
+++ Space Orbit 2/Assets/Scripts/Player/BasicMovement.cs
@@ -7,7 +7,7 @@ using UnityEngine.PlayerLoop;
 public class BasicMovement : MonoBehaviour
 {
 
-    Rigidbody rb;
+    Rigidbody2D rb;
 
     Vector2 rawInput = Vector2.zero;
 
@@ -17,54 +17,126 @@ public class BasicMovement : MonoBehaviour
     [SerializeField] public float currentSpeed = 0;
 
     [Header("Drive Settings")]
-    [SerializeField] private float _initialSpeed = 8;
-    [SerializeField] private float _accelerationFactor = 1;
-    [SerializeField] private float _decelerationFactor = 1;
-    [SerializeField] private float _steeringFactor = 10;
+    [SerializeField] private float _initialSpeed = 8f;
+
+    [SerializeField] private float _steerFactor = 1.6f;
+    [SerializeField] private float _thrustFactor = 16f;
+
+    [SerializeField] private float _accelerationFactor = 1f;
+    [SerializeField] private float _decelerationFactor = 4f;
+
+    [SerializeField] private float _maxThrustSpeed = 100f;
+    [SerializeField] private float _maxRotationSpeed = 1f;
+
+    [SerializeField] private float _speedDrag = 0.0000f;
+    [SerializeField] private float _angularDrag = 0.0000f;
+
+
+    private float _steerInput;
+    private float _thrustInput;
+
+    private Vector2 _currentVelocity;
+    private Vector2 _desiredVelocity;
+
+    private Vector2 _inputDirection;
+    private Vector2 _currentDirection;
+    private Vector2 _desiredDirection;
+
+    private Vector2 _thrustForce;
+    private Vector2 _steerForce;
+    private Vector2 _totalForce;
+    private float _steerTorque;
 
     void Start()
     {
-        rb = GetComponent<Rigidbody>();
-        rb.velocity = Vector3.forward * _initialSpeed;
+        rb = GetComponent<Rigidbody2D>();
+        rb.velocity = transform.forward * _initialSpeed;
 
         graphics = transform.GetChild(0).gameObject;
     }
 
     void Update()
     {
-        // Save heading direction to avoid recalculating each frame
-        // Might need to move to fixed update
-        // graphics.transform.rotation = Quaternion.LookRotation(rb.velocity, Vector3.up);
+        // Adjust linear and angular drag to control speed and rotation.
+        rb.drag = _speedDrag;
+        rb.angularDrag = _angularDrag;
+
+        _thrustInput = rawInput.y;
+        _steerInput = -rawInput.x;
+
+        _inputDirection = rawInput;
+
+        _currentDirection = transform.forward;
     }
 
     void FixedUpdate()
     {
-        if (rawInput.x < 0) 
-        {
-            rb.AddForce(graphics.transform.TransformDirection(Vector3.left * _steeringFactor * Mathf.Clamp(Mathf.Log(rb.velocity.magnitude, 3), 1, 10)));
-        }
-        else if (rawInput.x > 0) 
-        {
-            rb.AddForce(graphics.transform.TransformDirection(Vector3.right * _steeringFactor * Mathf.Clamp(Mathf.Log(rb.velocity.magnitude, 3), 1, 10)));
-        }
+        _currentVelocity = rb.velocity;
 
-        if(rawInput.y < 0)
-        {
-            rb.AddForce(graphics.transform.TransformDirection(Vector3.back * rb.velocity.magnitude * _accelerationFactor));
-        }
-        if (rawInput.y > 0) 
-        {
-            rb.AddForce(graphics.transform.TransformDirection(Vector3.forward * rb.velocity.magnitude * _decelerationFactor));
-        }
-
+        PhysicsMovement();
+        //NoPhysicsMovement();
 
         currentSpeed = rb.velocity.magnitude;
-
-        graphics.transform.rotation = Quaternion.LookRotation(rb.velocity, Vector3.up);
     }
 
     void OnMoveInput(InputValue value)
     {
         rawInput = value.Get<Vector2>();
+    }
+
+    void OldMovement()
+    {
+        // OOPS I BROKE IT?
+        if (rawInput.x < 0) 
+        {
+            rb.AddForce(graphics.transform.TransformDirection(Vector2.left * _steerFactor * Mathf.Clamp(Mathf.Log(rb.velocity.magnitude, 3), 1, 10)));
+        }
+        else if (rawInput.x > 0) 
+        {
+            rb.AddForce(graphics.transform.TransformDirection(Vector2.right * _steerFactor * Mathf.Clamp(Mathf.Log(rb.velocity.magnitude, 3), 1, 10)));
+        }
+
+        if(rawInput.y < 0)
+        {
+            rb.AddForce(graphics.transform.TransformDirection(Vector2.down * rb.velocity.magnitude * _accelerationFactor));
+        }
+        if (rawInput.y > 0) 
+        {
+            rb.AddForce(graphics.transform.TransformDirection(Vector2.up * rb.velocity.magnitude * _decelerationFactor));
+        }
+
+
+        currentSpeed = rb.velocity.magnitude;
+
+        graphics.transform.rotation = Quaternion.LookRotation(rb.velocity, Vector2.up);
+    }
+
+    void PhysicsMovement()
+    {
+        if (_thrustInput != 0)
+        {
+            if (_thrustInput > 0)
+            {
+                _thrustForce = _thrustFactor * _accelerationFactor * _currentDirection;
+            }
+            else if (_thrustInput < 0)
+            {
+                _thrustForce = _thrustFactor * _decelerationFactor * -_currentDirection;
+            }
+
+            rb.AddForce(_thrustForce, ForceMode2D.Force);
+        }
+
+        if (_steerInput != 0)
+        {
+            rb.rotation += _steerInput * _steerFactor;
+            _desiredVelocity = _currentVelocity.magnitude * _currentDirection.normalized;
+            rb.velocity = _desiredVelocity;
+        }
+    }
+
+    void NoPhysicsMovement()
+    {
+        
     }
 }
