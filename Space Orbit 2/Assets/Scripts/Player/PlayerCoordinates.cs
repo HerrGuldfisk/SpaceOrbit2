e40511317e8254cbaa01128f8d6b58ba74b22439
OscarWirdelius <oscar.wirdelius@gmail.com>
2023-09-30 23:32:48 +0200

    Updated Test Scene

--- Space Orbit 2/Assets/Scripts/Player/PlayerCoordinates.cs
+++ Space Orbit 2/Assets/Scripts/Player/PlayerCoordinates.cs
@@ -14,7 +14,7 @@ public class PlayerCoordinates : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        _target = transform;
     }
 
     // Update is called once per frame
