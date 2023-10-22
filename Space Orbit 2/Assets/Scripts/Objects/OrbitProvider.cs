0b8d7cc9a8786b5b12db73c2f4033ddd51b5b9cc
OscarWirdelius <oscar.wirdelius@gmail.com>
2023-10-22 22:16:18 +0200

    Working

--- Space Orbit 2/Assets/Scripts/Objects/OrbitProvider.cs
+++ Space Orbit 2/Assets/Scripts/Objects/OrbitProvider.cs
@@ -4,7 +4,10 @@ using System.Collections.ObjectModel;
 using UnityEngine;
 using UnityEditor;
 using Unity.VisualScripting;
+using static UnityEngine.GraphicsBuffer;
+using UnityEngine.UIElements;
 
+[ExecuteInEditMode]
 public class OrbitProvider : MonoBehaviour
 {
     [SerializeField]
@@ -18,6 +21,67 @@ public class OrbitProvider : MonoBehaviour
     [SerializeField]
     public void AddNewOrbiting(GameObject _orbitingObject, Transform _target, float _circleRadius, float _rotationSpeed, float _startAngle, bool _useTargetCoordinateSystem, bool _lookAtTarget, float _angle)
     {
-        OrbitingObjects.Add(new Orbiting(_orbitingObject, _target, _circleRadius, _rotationSpeed, _startAngle, _useTargetCoordinateSystem, _lookAtTarget, _angle));
+        Orbiting newOrbiting = new Orbiting(_orbitingObject, _target, _circleRadius, _rotationSpeed, _startAngle, _useTargetCoordinateSystem, _lookAtTarget, _angle);
+        OrbitingObjects.Add(newOrbiting);
     }
+
+    private void LateUpdate()
+    {
+        foreach(var o in OrbitingObjects)
+        {
+            o.UpdatePosition();
+        }
+    }
+
+#if UNITY_EDITOR
+
+    [SerializeField]
+    private bool drawGizmos = true;
+
+    private void OnDrawGizmos()
+    {
+        if (!drawGizmos)
+            return;
+
+        foreach (var o in OrbitingObjects)
+        {
+            // Draw an arc around the target
+            Vector3 position = o.target != null ? o.target.position : Vector3.zero;
+            Vector3 normal = Vector3.forward;
+            Vector3 forward = Vector3.up;
+            Vector3 labelPosition;
+
+            Vector3 positionOffset = o.ComputePositionOffset(o.startAngle);
+            Vector3 verticalOffset;
+
+
+            if (o.target != null && o.useTargetCoordinateSystem)
+            {
+                normal = o.target.forward;
+                forward = o.target.up;
+            }
+            verticalOffset = positionOffset.y * normal;
+
+            // Draw label to indicate elevation
+            if (Mathf.Abs(positionOffset.y) > 0.1)
+            {
+                UnityEditor.Handles.DrawDottedLine(position, position + verticalOffset, 5);
+                labelPosition = position + verticalOffset * 0.5f;
+                labelPosition += Vector3.Cross(verticalOffset.normalized, o.target != null && o.useTargetCoordinateSystem ? o.target.forward : Vector3.forward) * 0.25f;
+            }
+
+            position += verticalOffset;
+            positionOffset -= verticalOffset;
+
+            UnityEditor.Handles.DrawWireArc(position, normal, forward, 360, o.circleRadius);
+
+            // Draw label to indicate radius
+            UnityEditor.Handles.DrawLine(position, position + positionOffset);
+            labelPosition = position + positionOffset * 0.5f;
+            labelPosition += Vector3.Cross(positionOffset.normalized, o.target != null && o.useTargetCoordinateSystem ? o.target.forward : Vector3.forward) * 0.25f;
+            UnityEditor.Handles.Label(labelPosition, o.circleRadius.ToString("0.00"));
+        }
+    }
+
+#endif
 }
--- Space Orbit 2/Assets/Scripts/Objects/Orbiting.cs
+++ Space Orbit 2/Assets/Scripts/Objects/Orbiting.cs
@@ -13,11 +13,14 @@ public class Orbiting
     public float startAngle = 0;
     public bool useTargetCoordinateSystem = false;
     public bool lookAtTarget = false;
-    private float angle;
+    public float angle;
 
     public Orbiting(GameObject _orbitingObject, Transform _target, float _circleRadius, float _rotationSpeed, float _startAngle, bool _useTargetCoordinateSystem, bool _lookAtTarget, float _angle)
     {
         orbitingObject = _orbitingObject;
+        /* Replace with this?
+        orbitingObject = GameObject.Instantiate(_orbitingObject) as GameObject;
+        */
         target = _target;
         circleRadius = _circleRadius;
         rotationSpeed = _rotationSpeed;
@@ -27,7 +30,24 @@ public class Orbiting
         angle = _angle;
     }
 
-    private Vector3 ComputePositionOffset(float a)
+    public void UpdatePosition()
+    {
+        // Define the position the object must rotate around
+        Vector3 position = target != null ? target.position : Vector3.zero;
+
+        Vector3 positionOffset = ComputePositionOffset(angle);
+
+        // Assign new position
+        orbitingObject.transform.position = position + positionOffset;
+
+        // Rotate object so as to look at the target
+        if (lookAtTarget)
+            orbitingObject.transform.rotation = Quaternion.LookRotation(position - orbitingObject.transform.position, target == null ? Vector3.up : target.forward);
+
+        angle += Time.deltaTime * rotationSpeed;
+    }
+
+    public Vector3 ComputePositionOffset(float a)
     {
         a *= Mathf.Deg2Rad;
 
@@ -45,53 +65,4 @@ public class Orbiting
 
         return positionOffset;
     }
-
-#if UNITY_EDITOR
-
-    [SerializeField]
-    private bool drawGizmos = true;
-
-    private void OnDrawGizmos()
-    {
-        if (!drawGizmos)
-            return;
-
-        // Draw an arc around the target
-        Vector3 position = target != null ? target.position : Vector3.zero;
-        Vector3 normal = Vector2.up;
-        Vector3 forward = Vector3.forward;
-        Vector3 labelPosition;
-
-        Vector3 positionOffset = ComputePositionOffset(startAngle);
-        Vector3 verticalOffset;
-
-
-        if (target != null && useTargetCoordinateSystem)
-        {
-            normal = target.up;
-            forward = target.forward;
-        }
-        verticalOffset = positionOffset.y * normal;
-
-        // Draw label to indicate elevation
-        if (Mathf.Abs(positionOffset.y) > 0.1)
-        {
-            UnityEditor.Handles.DrawDottedLine(position, position + verticalOffset, 5);
-            labelPosition = position + verticalOffset * 0.5f;
-            labelPosition += Vector3.Cross(verticalOffset.normalized, target != null && useTargetCoordinateSystem ? target.forward : Vector3.forward) * 0.25f;
-        }
-
-        position += verticalOffset;
-        positionOffset -= verticalOffset;
-
-        UnityEditor.Handles.DrawWireArc(position, normal, forward, 360, circleRadius);
-
-        // Draw label to indicate radius
-        UnityEditor.Handles.DrawLine(position, position + positionOffset);
-        labelPosition = position + positionOffset * 0.5f;
-        labelPosition += Vector3.Cross(positionOffset.normalized, target != null && useTargetCoordinateSystem ? target.up : Vector3.up) * 0.25f;
-        UnityEditor.Handles.Label(labelPosition, circleRadius.ToString("0.00"));
-    }
-
-#endif
 }
