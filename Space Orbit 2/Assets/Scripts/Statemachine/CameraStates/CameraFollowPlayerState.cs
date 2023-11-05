7cdefddd3bcb7a951c1718821582f1789aba0fc5
douglasdriving <41374220+douglasdriving@users.noreply.github.com>
2023-11-05 12:27:56 -0600

    cam error fix
    no longer tries to follow player after death

--- Space Orbit 2/Assets/Scripts/Statemachine/CameraStates/CameraFollowPlayerState.cs
+++ Space Orbit 2/Assets/Scripts/Statemachine/CameraStates/CameraFollowPlayerState.cs
@@ -2,8 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CameraFollowPlayerState : BaseState
-{
+public class CameraFollowPlayerState : BaseState {
     public BasicMovement Player { get; private set; }
     public Camera Camera { get; private set; }
 
@@ -13,60 +12,55 @@ public class CameraFollowPlayerState : BaseState
     private float _transitionTime = 1;
     private Vector3 _startPosition;
 
-    public CameraFollowPlayerState(BasicMovement player, Camera camera) : base() 
-    {
+    public CameraFollowPlayerState(BasicMovement player, Camera camera) : base() {
         Player = player;
         Camera = camera;
     }
 
-    public override void Enter()
-    {
+    public override void Enter() {
         _transitionTime = 0;
         _startOrthoSize = Camera.orthographicSize;
         _startPosition = Camera.transform.position;
     }
 
-    public override void Execute()
-    {
-        if (_transitionTime < 1) 
-        {
-            TransitionToPlayer();
-            _transitionTime += Time.deltaTime / 1.2f;
-        }
-        else
-        {
-            FollowPlayer();
+    public override void Execute() {
+        if (Player) {
+            if (_transitionTime < 1) {
+                TransitionToPlayer();
+                _transitionTime += Time.deltaTime / 1.2f;
+            }
+            else {
+                FollowPlayer();
+            }
         }
     }
 
-    public override void Exit()
-    {
-        
+    public override void Exit() {
     }
 
-    private void TransitionToPlayer()
-    {
-        Camera.transform.position = Vector3.Lerp(_startPosition, LerpPosition(_transitionTime) , EaseInOutQuad(_transitionTime));
+    private void TransitionToPlayer() {
+        Camera.transform.position =
+            Vector3.Lerp(_startPosition, LerpPosition(_transitionTime), EaseInOutQuad(_transitionTime));
         Camera.orthographicSize = Mathf.Lerp(_startOrthoSize, _cameraSizetarget, EaseInOutQuad(_transitionTime));
     }
 
-    private void FollowPlayer()
-    {
+    private void FollowPlayer() {
         Camera.transform.position = LerpPosition();
-        // Player.transform.position + (Vector3)Player.rb.velocity.normalized * Mathf.Pow(Player.currentSpeed + 1, 0.95f);
     }
 
-    private Vector3 LerpPosition()
-    {
-        return Vector3.Lerp(Camera.transform.position, Player.transform.position + (Vector3)Player.rb.velocity.normalized * Mathf.Pow(Player.currentSpeed, 0.98f), Time.deltaTime * 2 );
-    }
-    private Vector3 LerpPosition(float transitionTime)
-    {
-        return Vector3.Lerp(Camera.transform.position, Player.transform.position + (Vector3)Player.rb.velocity.normalized * Mathf.Pow(Player.currentSpeed, 0.98f), 1 - transitionTime);
+    private Vector3 LerpPosition() {
+        return Vector3.Lerp(Camera.transform.position,
+            Player.transform.position + (Vector3)Player.rb.velocity.normalized * Mathf.Pow(Player.currentSpeed, 0.98f),
+            Time.deltaTime * 2);
     }
 
-    float EaseInOutQuad(float x)
-    {
-        return x < 0.5 ? 2 * x* x : 1 - Mathf.Pow(-2 * x + 2, 2) / 2;
+    private Vector3 LerpPosition(float transitionTime) {
+        return Vector3.Lerp(Camera.transform.position,
+            Player.transform.position + (Vector3)Player.rb.velocity.normalized * Mathf.Pow(Player.currentSpeed, 0.98f),
+            1 - transitionTime);
+    }
+
+    float EaseInOutQuad(float x) {
+        return x < 0.5 ? 2 * x * x : 1 - Mathf.Pow(-2 * x + 2, 2) / 2;
     }
 }
