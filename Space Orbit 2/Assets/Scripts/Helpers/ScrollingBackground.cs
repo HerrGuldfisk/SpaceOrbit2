8744800c0b5c7582846083e1fb3c9f4c468feedb
OscarWirdelius <oscar.wirdelius@gmail.com>
2023-11-13 23:26:15 +0100

    bg implement start

---
+++ Space Orbit 2/Assets/Scripts/Helpers/ScrollingBackground.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScrollingBackground : MonoBehaviour
+{
+    [SerializeField] private Renderer _bgRenderer;
+    [SerializeField] private float _speed;
+
+    void Update()
+    {
+        
+    }
+}
