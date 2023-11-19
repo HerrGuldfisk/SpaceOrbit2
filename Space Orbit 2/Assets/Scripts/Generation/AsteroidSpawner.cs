596afcf26fdd54b5c194beb006286dd04b85ef25
douglasdriving <41374220+douglasdriving@users.noreply.github.com>
2023-11-19 12:55:37 -0300

    world get - asteroid count adjust
    made it so that it generates different number of asteroids depending on the size of the gravity field, making sure it all fits

--- Space Orbit 2/Assets/Scripts/Generation/AsteroidSpawner.cs
+++ Space Orbit 2/Assets/Scripts/Generation/AsteroidSpawner.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Serialization;
 using Random = UnityEngine.Random;
 
 [RequireComponent(typeof(PlanetSettings))]
@@ -9,37 +10,64 @@ public class AsteroidSpawner : MonoBehaviour {
     [SerializeField] private GameObject asteroidPrefab;
     private PlanetSettings _planetSettings;
 
+    [SerializeField] private int maxNumberOfAsteroids = 10;
+    [SerializeField] private int minNumberOfAsteroids = 0;
+    [SerializeField] private float maxAsteroidSize = 10;
+    [SerializeField] private float minAsteroidSize = 1;
+
     private void Awake() {
         _planetSettings = GetComponent<PlanetSettings>();
     }
 
     public void SpawnAsteroidsInOrbit() {
-        int numberOfAsteroids = Random.Range(0, 10); //should number depend on gravity field size???
-        float bodySize = _planetSettings.GetBodySize();
-        float gravityFieldSize = _planetSettings.GetGravityFieldSize();
-        for (int i = 0; i < numberOfAsteroids; i++) {
-            SpawnAsteroidAtRandomPosition(bodySize, gravityFieldSize);
+        float planetBodyDiameter = _planetSettings.GetBodySize();
+        float gravityFieldDiameter = _planetSettings.GetGravityFieldSize();
+
+        float planetBodyRadius = planetBodyDiameter / 2;
+        float gravityFieldRadius = gravityFieldDiameter / 2;
+        
+        int numberOfAsteroidsToSpawn = GenerateNumberOfAsteroidsToSpawn(planetBodyRadius, gravityFieldRadius);
+
+        for (int i = 0; i < numberOfAsteroidsToSpawn; i++) {
+            SpawnAsteroidAtRandomPosition(planetBodyRadius, gravityFieldRadius);
         }
     }
 
-    private void SpawnAsteroidAtRandomPosition(float bodySize, float gravityFieldSize) {
+    private int GenerateNumberOfAsteroidsToSpawn(float planetBodyRadius, float gravityFieldRadius) {
+        int randomAsteroidNumber =
+            Random.Range(minNumberOfAsteroids, maxNumberOfAsteroids);
+        
+        float distanceFromPlanetSurfaceToGravityFieldEdge = gravityFieldRadius - planetBodyRadius;
+
+        int numberOfFullSizeAsteroidsThatCanFitInGravityField =
+            Mathf.FloorToInt(distanceFromPlanetSurfaceToGravityFieldEdge / maxAsteroidSize);
+
+        int numberOfAsteroidsToSpawn = Mathf.Clamp(randomAsteroidNumber, minNumberOfAsteroids,
+            numberOfFullSizeAsteroidsThatCanFitInGravityField);
+        return numberOfAsteroidsToSpawn;
+    }
+
+    private void SpawnAsteroidAtRandomPosition(float planetBodyRadius, float gravityFieldRadius) {
         //spawn at random position
-        Vector3 spawnPos = GetRandomPosInGravityField(bodySize, gravityFieldSize);
+        Vector3 spawnPos = GetRandomPosInGravityField(planetBodyRadius, gravityFieldRadius);
         GameObject asteroid = Instantiate(asteroidPrefab, spawnPos, Quaternion.identity);
 
         //randomize its size
-        float localScaleValue = Random.Range(1, 10); // depend on gravity field size?
-        asteroid.transform.localScale = new Vector3(localScaleValue, localScaleValue, localScaleValue);
+        float asteroidLocalScaleValue = Random.Range(minAsteroidSize, maxAsteroidSize);
+        asteroid.transform.localScale =
+            new Vector3(asteroidLocalScaleValue, asteroidLocalScaleValue, asteroidLocalScaleValue);
 
         //++set in orbit
     }
 
-    private Vector3 GetRandomPosInGravityField(float bodySize, float gravityFieldSize) {
-        float distanceFromPlanetCenter = Random.Range(bodySize/2, gravityFieldSize/2);
-        Vector3 directionVectorFromPlanetCenter = new Vector3(Random.Range(-1f, 1f), Random.Range(-1f, 1f), 0).normalized;
-        Vector3 positionInRelationToPlanetCenter = directionVectorFromPlanetCenter * distanceFromPlanetCenter;
+    private Vector3 GetRandomPosInGravityField(float planetBodyRadius, float gravityFieldRadius) {
+        float minDistanceFromPlanetCenter = planetBodyRadius + maxAsteroidSize;
+        float maxDistanceFromPlanetCenter = gravityFieldRadius;
+        float randomDistanceFromPlanetCenter = Random.Range(minDistanceFromPlanetCenter, maxDistanceFromPlanetCenter);
+        Vector3 directionVectorFromPlanetCenter =
+            new Vector3(Random.Range(-1f, 1f), Random.Range(-1f, 1f), 0).normalized;
+        Vector3 positionInRelationToPlanetCenter = directionVectorFromPlanetCenter * randomDistanceFromPlanetCenter;
         Vector3 worldPosition = transform.position + positionInRelationToPlanetCenter;
         return worldPosition;
-        //problem here is that asteroids can be RIGHT on the edge. should at least the distance of the biggest possible asteroid size
     }
 }
