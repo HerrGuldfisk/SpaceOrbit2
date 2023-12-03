7bf8809a95b55def552613572b397c3d0f4b791c
douglasdriving <41374220+douglasdriving@users.noreply.github.com>
2023-12-03 12:50:51 -0300

    pointer toggle
    hiding the pointer while the target object is visible on screen

--- Space Orbit 2/Assets/PingSystem/Pointer.cs
+++ Space Orbit 2/Assets/PingSystem/Pointer.cs
@@ -1,25 +1,49 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 //points to an object off-screen, and hides if the object is on-screen
+[RequireComponent(typeof(Image))]
 public class Pointer : MonoBehaviour {
     [SerializeField] Transform target;
+    Renderer targetRenderer;
     Camera mainCamera;
     private float canvasWidth;
     private float canvasHeight;
+    private Image pointerImage => GetComponent<Image>();
 
     void Start() {
         mainCamera = Camera.main;
+        targetRenderer = target.GetComponentInChildren<Renderer>();
+        Debug.Assert(targetRenderer != null, "no renderer found on target");
         Canvas canvas = GetComponentInParent<Canvas>();
         canvasWidth = canvas.pixelRect.width;
         canvasHeight = canvas.pixelRect.height;
     }
 
     void Update() {
+        if (!target || !targetRenderer) {
+            return;
+        }
+
+        //check if the target is on the main cam
         Vector3 objectPosOnScreen = mainCamera.WorldToViewportPoint(target.position);
-        float pointerX = Mathf.Clamp(objectPosOnScreen.x, 0.1f, 0.9f);
-        float pointerY = Mathf.Clamp(objectPosOnScreen.y, 0.1f, 0.9f);
+        bool targetIsOnScreen = objectPosOnScreen.x > 0 && objectPosOnScreen.x < 1 && objectPosOnScreen.y > 0 &&
+                                objectPosOnScreen.y < 1;
+
+        if (targetIsOnScreen) {
+            pointerImage.enabled = false;
+        }
+        else {
+            pointerImage.enabled = true;
+            PositionPointer(objectPosOnScreen);
+        }
+    }
+
+    private void PositionPointer(Vector3 targetPosOnScreen) {
+        float pointerX = Mathf.Clamp(targetPosOnScreen.x, 0.1f, 0.9f);
+        float pointerY = Mathf.Clamp(targetPosOnScreen.y, 0.1f, 0.9f);
         float pointerXInCanvasScale = pointerX * canvasWidth;
         float pointerYInCanvasScale = pointerY * canvasHeight;
         transform.position = new Vector3(pointerXInCanvasScale, pointerYInCanvasScale, 0);
